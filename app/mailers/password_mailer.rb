dd5f82c3e958494edf9b9d9ad2e5ee32a2d12ccf
Rony Francis <ronydfrnd@gmail.com>
2019-06-14 07:06:58 +0530

    mail correction5

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,6 +2,7 @@ class PasswordMailer < ApplicationMailer
   default from: 'support@businessbooks.com'
   def send_recover_email
     @user = User.find_by(email: params[:email])
+    attachments['logo.png'] = File.read(Rails.root.join('app', 'assets', 'images','businessbooks.png'))
     mail(to: @user.email, subject: 'Temporary Password Created')
   end
 end
