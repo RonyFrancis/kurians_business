24a671d6eea5658b28effa09faa406c2768198e8
Rony Francis <ronydfrnd@gmail.com>
2019-06-14 06:15:10 +0530

    logo for mailer

--- app/mailers/bill_mailer.rb
+++ app/mailers/bill_mailer.rb
@@ -2,6 +2,7 @@ class BillMailer < ApplicationMailer
   def send_bill_mail
     @user = User.find_by(email: params[:email])
     @bill = @user.bills.last
+    attachments['logo.png'] = Rails.root.join('app', 'assets', 'images','businessbooks.png')
     attachments['bill_image.jpg'] = File.read(@bill.avatar.current_path)
     mail(to: @user.email, subject: 'Your Bill')
   end
@@ -10,6 +11,7 @@ class BillMailer < ApplicationMailer
     @user = User.find_by(email: params[:email])
     @bill = @user.bills.last
     @admin = User.find_by(is_admin: true)
+    attachments['logo.png'] = Rails.root.join('app', 'assets', 'images','businessbooks.png')
     attachments['bill_image.jpg'] = File.read(@bill.avatar.current_path)
     mail(to: @admin.email, subject: "#{@user.email}'s Bill")
   end
