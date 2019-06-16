87ce24d6d4b45b204597a01dd62d72e9a96eb755
Rony Francis <ronydfrnd@gmail.com>
2019-06-16 06:26:16 +0530

    mailer update

--- app/mailers/bill_mailer.rb
+++ app/mailers/bill_mailer.rb
@@ -2,8 +2,10 @@ class BillMailer < ApplicationMailer
   def send_bill_mail
     @user = User.find_by(email: params[:email])
     @bill = @user.bills.last
-    attachments['logo.png'] = File.read(Rails.root.join('app', 'assets', 'images','businessbooks.png'))
+    attachments['logo.png'] = File.read(app_logo)
     attachments['bill_image.jpg'] = File.read(@bill.avatar.current_path)
+    attachments['apple.png'] = File.read(apple_logo)
+    attachments['play_store.png'] = File.read(play_store_logo)
     mail(to: @user.email, subject: 'Your Bill')
   end
 
@@ -11,8 +13,10 @@ class BillMailer < ApplicationMailer
     @user = User.find_by(email: params[:email])
     @bill = @user.bills.last
     @admin = User.find_by(is_admin: true)
-    attachments['logo.png'] = File.read(Rails.root.join('app', 'assets', 'images','businessbooks.png'))
+    attachments['logo.png'] = File.read(app_logo)
     attachments['bill_image.jpg'] = File.read(@bill.avatar.current_path)
+    attachments['apple.png'] = File.read(apple_logo)
+    attachments['play_store.png'] = File.read(play_store_logo)
     mail(to: @admin.email, subject: "#{@user.email}'s Bill")
   end
 end
--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,7 +2,9 @@ class PasswordMailer < ApplicationMailer
   default from: 'support@businessbooks.com'
   def send_recover_email
     @user = User.find_by(email: params[:email])
-    attachments['logo.png'] = File.read(Rails.root.join('app', 'assets', 'images','businessbooks.png'))
+    attachments['logo.png'] = File.read(app_logo)
+    attachments['apple.png'] = File.read(apple_logo)
+    attachments['play_store.png'] = File.read(play_store_logo)
     mail(to: @user.email, subject: 'Temporary Password Created')
   end
 end
