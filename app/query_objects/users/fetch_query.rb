796c70ff22c57e5d59adf5143ced0ced3ddc41c2
Antony Rony <antony.rony@sellerapp.com>
2019-12-07 15:29:47 +0530

    add status to list api

--- app/query_objects/users/fetch_query.rb
+++ app/query_objects/users/fetch_query.rb
@@ -8,18 +8,16 @@ module Users
 
     def call
       raise InvalidParamsError unless valid_params?
-      puts "*" * 40
-      puts search_term
       if search_term.blank?
       users = User.all.select(
-        "id", "first_name", "last_name", "email", "forward_email",
+        "id", "first_name", "last_name", "email", "forward_email", "status",
         "mobile_number", "is_admin", "created_at", "updated_at"
       ).paginate(page: page).order('created_at DESC')
     else
       users = User.where(
       "first_name like ? or last_name like ? or email like ? or forward_email like ? or mobile_number like ?",
        "%" + search_term + "%","%" + search_term + "%","%" + search_term + "%","%" + search_term + "%","%" + search_term + "%" ).select(
-        "id", "first_name", "last_name", "email", "forward_email",
+        "id", "first_name", "last_name", "email", "forward_email", "status",
         "mobile_number", "is_admin", "created_at", "updated_at"
       ).paginate(page: page).order('created_at DESC')
     end
