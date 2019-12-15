a9aab93fa66db715a4f637bb094d5470e5ddc486
Antony Rony <antony.rony@sellerapp.com>
2019-12-15 22:01:37 +0530

    company_name added

--- app/query_objects/users/fetch_query.rb
+++ app/query_objects/users/fetch_query.rb
@@ -11,14 +11,14 @@ module Users
       if search_term.blank?
       users = User.all.select(
         "id", "first_name", "last_name", "email", "forward_email", "status",
-        "mobile_number", "is_admin", "created_at", "updated_at"
+        "mobile_number", "is_admin", "created_at", "updated_at", "user_type", "company_name"
       ).paginate(page: page).order('created_at DESC')
     else
       users = User.where(
-      "first_name like ? or last_name like ? or email like ? or forward_email like ? or mobile_number like ?",
-       "%" + search_term + "%","%" + search_term + "%","%" + search_term + "%","%" + search_term + "%","%" + search_term + "%" ).select(
+      "first_name like ? or last_name like ? or email like ? or forward_email like ? or mobile_number like ? or company_name like ?",
+       "%" + search_term + "%","%" + search_term + "%","%" + search_term + "%","%" + search_term + "%","%" + search_term + "%", "%" + search_term + "%" ).select(
         "id", "first_name", "last_name", "email", "forward_email", "status",
-        "mobile_number", "is_admin", "created_at", "updated_at"
+        "mobile_number", "is_admin", "created_at", "updated_at","user_type", "company_name"
       ).paginate(page: page).order('created_at DESC')
     end
       [users, pagination_details(users)]
