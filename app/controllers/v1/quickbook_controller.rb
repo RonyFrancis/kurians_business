a70f8f604ff9a912702e7aeccfe2de4a71921525
Govind K U <govind.ku@mobme.in>
2020-01-21 23:43:32 +0530

    Fix api headers

--- app/controllers/v1/quickbook_controller.rb
+++ app/controllers/v1/quickbook_controller.rb
@@ -1,6 +1,8 @@
 module V1
   class QuickbookController < ApplicationController
     require 'rest-client'
+    require "uri"
+    require "net/http"
     skip_before_action :verify_authenticity_token
 
     def prepare_auth_code_url
@@ -11,13 +13,14 @@ module V1
     def redirect_url
       tokens = QuickBook::AuthToken.new(code: params[:code]).call
       newToken = QuickBook::RefreshToken.new(refresh_tokens: tokens.refresh_token).call
-      url = "https://quickbooks.api.intuit.com/v3/company/123146329989269/companyinfo/" + params[:realmId] + "?minorversion=42"
-      resp = RestClient::Request.execute(method: :get, url: url,
-                            headers: {
-                              :Authorization =>  newToken.access_token,
-                              :ContentType => 'application/json'
-                            })
-      render json: {"resp": resp}
+      url = URI("https://quickbooks.api.intuit.com/v3/company/" + params[:realmId] + "/companyinfo/" + params[:realmId] + "?minorversion=42")
+      https = Net::HTTP.new(url.host, url.port);
+      https.use_ssl = true
+      request = Net::HTTP::Get.new(url)
+      request["Accept"] = "application/json"
+      request["Content-Type"] = "application/json"
+      request["Authorization"] = "Bearer #{newToken.access_token}"
+      response = https.request(request)
     end
 
     def get_auth_token
